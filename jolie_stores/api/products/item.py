99e6e95aae9444f5060aedba22bcfde42c3011c2
Hagar <hagermossad80@gmail.com>
2024-07-29 10:44:43 +0300

    add city in create cart api

--- jolie_stores/api/products/item.py
+++ jolie_stores/api/products/item.py
@@ -132,6 +132,7 @@ def create_order(carts , user):
     sales_order.user_phone = user["phone"]
     sales_order.user_email = user["email"]
     sales_order.user_address = user["address"]
+    sales_order.city = user["city"]
     sales_order.delivery_date = today()
     sales_order.date_time = now()
     for cart in carts :
