feb5057650f342372b0f4a6ac67c7459b5b73f36
Hagar <hagermossad80@gmail.com>
2024-07-31 13:49:49 +0300

    setup domain

--- jolie_stores/install.py
+++ jolie_stores/install.py
@@ -5,7 +5,7 @@ def after_install():
     create_domain_list()
 
 def create_domain_list():
-	if not frappe.db.exists("Domain", "IFI"):
+	if not frappe.db.exists("Domain", "Jolie"):
 		dm1 = frappe.new_doc("Domain")
-		dm1.domain = 'IFI'
+		dm1.domain = 'Jolie'
 		dm1.insert()
