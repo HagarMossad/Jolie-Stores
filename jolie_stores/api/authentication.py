b01093f5d1336544599a5dc7c0fb1b9543788766
Hagar <hagermossad80@gmail.com>
2024-08-01 13:10:59 +0300

    modify integration with rtx

--- jolie_stores/api/authentication.py
+++ jolie_stores/api/authentication.py
@@ -4,13 +4,16 @@ from frappe import _
 
 def get_token():
     authentication_doc = frappe.get_single("Authentication Setting")
-    token_url = authentication_doc.endpoinet
+    base_ul = frappe.get_doc("Integration System" , "RTX")
+    token_url = f"{base_ul.get('base_url')}{authentication_doc.endpoinet}"
     data = {
         "username" : authentication_doc.user,
         "password" : authentication_doc.password
     }
     response = requests.post(token_url, data)
     data = response.json()
-    authentication_doc.token =  data.get('token')
+    authentication_doc.db_set("token",data.get('token')) 
     authentication_doc.save()
-    return authentication_doc.token
+    frappe.db.commit()
+    print({"token" : authentication_doc.token , "base_url" :base_ul.get('base_url') })
+    return {"token" : authentication_doc.token , "base_url" :base_ul.get('base_url') }
--- jolie_stores/controllers/sales_order.py
+++ jolie_stores/controllers/sales_order.py
@@ -9,23 +9,20 @@ def submit_order(self , event):
     "address": self.user_address,
     "total_amount": self.total,
     "notes": "note",
-    "order_date": self.date_time,
+    "order_date": str(self.date_time),
     "shipment_contents": ' '.join([item.item_code for item in self.items]),
     "weight": "50.00",
     "city": int(frappe.get_doc("City" , self.city).id)
     }
-    print(data["city"])
-
-    token = get_token()
-    url = "http://92.205.177.106:8020/integration/order"
+    token = get_token()["token"]
+    url = f'{get_token()["base_url"]}/integration/order'
+    print(url)
     headers = {
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
-    
     response = requests.post(url, json=data, headers=headers)
     print(response)
-    print("*"*30)
     data = response.json()
 
 def calculate_fees(self , event):
